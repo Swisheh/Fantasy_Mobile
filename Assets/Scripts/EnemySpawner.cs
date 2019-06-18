e3e95e367cb79d1314e07d6c3cac7b93aaa7ca61
Rory <rory.hiscock@hotmail.com>
2019-06-18 20:41:53 +1000

    Day 2
    Player Control.
    Enemy Spawner.
    Health Bar.
    Camera Follow.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    private float spawnX;
+    private float spawnZ;
+
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnX = transform.localPosition.x;
+        spawnZ = transform.localPosition.z;
+
+        SpawnEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnEnemy()
+    {
+        GameObject enemyInstance;
+        enemyInstance = Instantiate(enemy);
+        enemyInstance.transform.position = new Vector3(transform.position.x + Random.Range(-spawnX, spawnX), 0, 
+            transform.position.z + Random.Range(-spawnZ, spawnZ));
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    #region Global Fields
+    [Tooltip("Player Health")]
+    [SerializeField]
+    private float playerHealth = 100;
+    [Tooltip("Player Speed")]
+    [SerializeField]
+    private float playerSpeed = 4;
+    [Tooltip("Player Anim Controller")]
+    [SerializeField]
+    private RuntimeAnimatorController playerAnimCont;
+
+    float horizontalMove;
+    float verticalMove;
+
+    private Rigidbody rb;
+    private Animator anim;
+    #endregion
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+        anim.runtimeAnimatorController = playerAnimCont;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+        Moving();
+    }
+
+    public void Moving()
+    {
+        if (horizontalMove > 0)
+        {
+            transform.Translate(Vector3.right * playerSpeed * Time.deltaTime, Space.World);
+        }
+        else if (horizontalMove < 0)
+        {
+            transform.Translate(Vector3.left * playerSpeed * Time.deltaTime, Space.World);
+        }
+
+        if (verticalMove > 0)
+        {
+            transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime, Space.World);            
+        }
+        else if (verticalMove < 0)
+        {
+            transform.Translate(Vector3.back * playerSpeed * Time.deltaTime, Space.World);            
+        }
+
+        if(horizontalMove != 0 || verticalMove != 0) 
+        {
+            Vector3 movement = new Vector3(horizontalMove, 0.0f, verticalMove);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F); // rotates to face direction moving
+
+            anim.SetBool("walking", true);
+        }
+        else
+        {
+            anim.SetBool("walking", false);
+        }
+    }
+}
